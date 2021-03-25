57d7617f31af3e08b9c1d63d248f182ec03b5d83
ds <1159629515@qq.com>
2021-03-26 00:07:13 +0800

    spring boot 自动配置

--- basic/src/main/java/com/ds/example/basic/AutoConfigApp.java
+++ basic/src/main/java/com/ds/example/basic/AutoConfigApp.java
@@ -14,6 +14,16 @@ import org.springframework.context.ConfigurableApplicationContext;
  *      4、从META-INF/spring.factories位置来加载一个文件。
  *      默认扫描我们当前系统里面所有META-INF/spring.factories位置的文件
  *     spring-boot-autoconfigure-2.3.4.RELEASE.jar包里面也有META-INF/spring.factories
+ *
+ *
+ *      • SpringBoot先加载所有的自动配置类  xxxxxAutoConfiguration
+ *      • 每个自动配置类按照条件进行生效，默认都会绑定配置文件指定的值。xxxxProperties里面拿。xxxProperties和配置文件进行了绑定
+ *      • 生效的配置类就会给容器中装配很多组件
+ *      • 只要容器中有这些组件，相当于这些功能就有了
+ *      • 定制化配置
+ *      • 用户直接自己@Bean替换底层的组件
+ *      • 用户去看这个组件是获取的配置文件什么值就去修改。
+ *      xxxxxAutoConfiguration ---> 组件  ---> xxxxProperties里面拿值  ----> application.properties
  */
 @SpringBootApplication
 public class AutoConfigApp {
